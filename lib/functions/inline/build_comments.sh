a1d11e548c701eed4cfbf9933e0dea1dc930157e
Ben Robeson <saiboogu@gmail.com>
2025-01-19 18:48:22 -0500

    Fixed build_comments

--- lib/functions/inline/build_comments.sh
+++ lib/functions/inline/build_comments.sh
@@ -13,10 +13,9 @@
 build_comments() {
         # Verify that metadata variables are populated before running.
         [[ $title != '' ]] && {
-                cat $config/comments.html > $1
-                
+                               
                 # Replace all tags in {{this format}} with their value. We do this using Bash pattern replacement.
-                comment_block="$(cat $1)"
+                comment_block="$(cat $config/comments.html)"
 
                 comment_block="${comment_block//\{\{title\}\}/"$title"}"
                 comment_block="${comment_block//\{\{base_url\}\}/"$base_url"}"
