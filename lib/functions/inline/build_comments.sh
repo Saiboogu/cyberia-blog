893000ac9d817a94ac39ed774ed99cd9de7bb0c5
Ben Robeson <saiboogu@gmail.com>
2025-01-19 19:05:30 -0500

    Update comments configuration and enhance dynamic comment building

--- lib/functions/inline/build_comments.sh
+++ lib/functions/inline/build_comments.sh
@@ -18,8 +18,16 @@ build_comments() {
                 comment_block="$(cat $config/comments.html)"
 
                 comment_block="${comment_block//\{\{title\}\}/"$title"}"
+                comment_block="${comment_block//\{\{author\}\}/"$author"}"
+                comment_block="${comment_block//\{\{description\}\}/"$description"}"
+                comment_block="${comment_block//\{\{language\}\}/"$language"}"
+                comment_block="${comment_block//\{\{thumbnail\}\}/"$thumbnail"}"
+                comment_block="${comment_block//\{\{published_date\}\}/"$published_date"}"
+                comment_block="${comment_block//\{\{modified_date\}\}/"$modified_date"}"
+                comment_block="${comment_block//\{\{canonical_url\}\}/"$canonical_url"}"
                 comment_block="${comment_block//\{\{base_url\}\}/"$base_url"}"
-
+                comment_block="${comment_block//\{\{relative_url\}\}/"$relative_url"}"
+                comment_block="${comment_block//\{\{global_name\}\}/"$global_name"}"
 
                 echo "$comment_block" >> $1
                 comment_block=""
