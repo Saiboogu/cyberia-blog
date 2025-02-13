1736739929d8dbfc12b15d304591d571672b112f
Ben Robeson <saiboogu@gmail.com>
2025-02-12 21:26:34 -0500

    Changed tab index in build_toc to skip redundant links, adjusted CSS for smaller screen sizes

--- lib/functions/subshell/build_toc.sh
+++ lib/functions/subshell/build_toc.sh
@@ -34,7 +34,7 @@ clear_metadata
 toc_tmp="arise-toc-$RANDOM.tmp"
 find . -mindepth 2 -maxdepth 2 -type f -name 'index.md' | while read fname; do
 get_page_metadata $fname
-echo '<tr class="arise-toc-tr"><td class="arise-toc-td"><a href="'"$relative_url"'">'"$published_date"'</a></td><td class="arise-toc-td"><a href="'"$relative_url"'">'"$title"'</a></td><td class="arise-toc-td-desc"><a href="'"$relative_url"'">'"$description"'</a></td></tr>' >> $toc_tmp
+echo '<tr class="arise-toc-tr"><td class="arise-toc-td"><a href="'"$relative_url"'">'"$published_date"'</a></td><td class="arise-toc-td"><a tabindex="-1" href="'"$relative_url"'">'"$title"'</a></td><td class="arise-toc-td-desc"><a tabindex="-1" href="'"$relative_url"'">'"$description"'</a></td></tr>' >> $toc_tmp
 clear_metadata
 done
 
